1ab0c8e5c2caa832a2a58798dfa990f970b1f2d9
Tarungoli12 <Tarun@6263>
2026-02-23 19:25:38 +0530

    2nd

--- src/test/java/com/automation/cucumber/TestNGTestRunner.java
+++ src/test/java/com/automation/cucumber/TestNGTestRunner.java
@@ -6,5 +6,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src//test//java//com//automation//cucumber//featureFiles",
         tags = "smoke", glue = "com.automation.cucumber.stepDefinitions",
         monochrome = true,plugin = {"html:target/cucumber.html"})
+
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 }
